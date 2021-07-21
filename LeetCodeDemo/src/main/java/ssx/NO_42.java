9bb758aee3c4ea2700435d6dba9666c69ba6a96c
shenshuxin01 <ssx17852015601@163.com>
2021-07-21 11:29:55 +0800

    LC-回复到42题

---
+++ LeetCodeDemo/src/main/java/ssx/NO_42.java
@@ -0,0 +1,18 @@
+package ssx;
+
+/**
+ *
+ *
+ */
+
+public class NO_42 {
+
+    public static void main(String[] args) {
+        long l = System.currentTimeMillis();
+
+        System.out.println(  );
+
+        System.out.println("执行时间："+(System.currentTimeMillis()-l)+"毫秒");
+    }
+
+}
